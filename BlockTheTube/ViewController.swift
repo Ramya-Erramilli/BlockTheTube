b86e420a0fdcf7cedfe0bee6a98ef1dc3679e56d
Ramya Erramilli <e.ramya.103@gmail.com>
2020-06-23 12:12:59 +0530

    Blocking youtube in all domains implemented

--- BlockTheTube/ViewController.swift
+++ BlockTheTube/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKNavigationDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        searchBar.delegate = self
+        searchBar.autocapitalizationType = .none
+        
+        webView.navigationDelegate = self
+        
+
     }
 
-
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if var text =  searchBar.text{
+            if !text.contains("https://"){
+                text = "https://\(text)"
+            }
+            if let url = URL(string: text){
+                let req = URLRequest(url: url)
+                self.webView.load(req)
+            }
+        }
+    }
+    @IBOutlet weak var mode: UISwitch!
+    @IBOutlet weak var modeLabel: UILabel!
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let host = navigationAction.request.url?.host {
+            if (host == "www.youtube.com" || host.contains("youtube")) && mode.isOn {
+                decisionHandler(.cancel)
+                self.performSegue(withIdentifier: "error", sender: self)
+                return
+            }
+        }
+        decisionHandler(.allow)
+    }
+    
+    @IBAction func modeSelector(_ sender: UISwitch) {
+        if !sender.isOn{
+            modeLabel.text = "Mode: Timer"
+            
+        }else{
+            modeLabel.text = "Mode: Blocked"
+        }
+    
+    }
+    
 }
+
 
